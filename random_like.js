8ace73d364cd8ecdebf66321e9b393682bc82da5
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-09-05 16:27:30 +0300

    random like added

---
+++ random_like.js
@@ -0,0 +1,1 @@
+javascript:(()=>{let l=[...document.querySelectorAll("div > span.widget_cnt.controls-list_lk.js-klass.js-klass-action.h-mod")];const t=l.length;l[Math.floor(Math.random()*Math.floor(t))].click()})();
