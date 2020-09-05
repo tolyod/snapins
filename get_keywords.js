8c9acf01401677b2249f9310b37cd8eec3f71ce6
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-09-05 16:28:54 +0300

    move get keywords to external link

--- get_keywords.js
+++ get_keywords.js
@@ -4,7 +4,9 @@ var getKeyWords = () => {
     [...divsKeyWords].map(el => {
         const elVws = el.querySelector('div[class="video-card_info"] span').innerText.replace(/\D/g,'');
         if (parseInt(elVws) > 200000) {
-            tagsTextRaw += " " + el.querySelector('div[class="video-card_n-w"] a').title.replace(/[^a-z\u0400-\u04FF]/gi, " ").trim().replace(/[\s]{1,}/g, ' ')
+          tagsTextRaw += " " + el.querySelector('div[class="video-card_n-w"] a')
+            .title.replace(/[^a-z\u0400-\u04FF]/gi, " ")
+            .trim().replace(/[\s]{1,}/g, ' ');
         }
     });
     let tags_pre = tagsTextRaw.trim().split(" ");
