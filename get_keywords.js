a4fc4473e7da8fb8727bc6d98310c44e899a37c3
anatoliy.poloz <1892972+tolyod@users.noreply.github.com>
2024-07-11 21:18:31 +0300

    Update get_keywords.js

--- get_keywords.js
+++ get_keywords.js
@@ -1,8 +1,8 @@
 var getKeyWords = () => {
-    let divsKeyWords = document.querySelectorAll('div.video-card');
+    let divsKeyWords = document.querySelectorAll('.ugrid_i.js-seen-item-movie');
     let tagsTextRaw = "";
     [...divsKeyWords].map(el => {
-        const elVws = el.querySelector('div[class="video-card_info"] span').innerText.replace(/\D/g,'');
+        const elVws = el.querySelector('.video-card_info.ellip > .video-card_info_i').innerText.replace(/\D/g,'');
         if (parseInt(elVws) > 200000) {
           tagsTextRaw += " " + el.querySelector('div[class="video-card_n-w"] a')
             .title.replace(/[^a-z\u0400-\u04FF]/gi, " ")
@@ -12,6 +12,7 @@ var getKeyWords = () => {
     let tags_pre = tagsTextRaw.trim().split(" ");
     let unique = [... new Set(tags_pre)].filter(s => s.length > 3);
     let tags_text = unique.join(', ');
+    console.log(tags_text);
     localStorage.setItem('oktag_keywords', JSON.stringify(tags_text))
 };
 getKeyWords();
