369e1f65bcb263fedc1d29d2215286de43b1ab88
anatoliy.poloz <1892972+tolyod@users.noreply.github.com>
2024-07-11 21:13:55 +0300

    Update get_names.js

--- get_names.js
+++ get_names.js
@@ -1,15 +1,21 @@
 var getKeyNames = () => {
-    let divsNames = document.querySelectorAll('div.video-card');
+    let divsNames = document.querySelectorAll('.ugrid_i.js-seen-item-movie');
     let tarr = [];
     let i = 0;
     [...divsNames].map(el => {
-        elVws = el.querySelector('div[class="video-card_info"] span').innerText.replace(/\D/g, '');
+        elVws = el.querySelector('.video-card_info.ellip > .video-card_info_i').innerText.replace(/\D/g, '');
         if (parseInt(elVws) > 200000) {
-          tarr[i] = el.querySelector('div[class="video-card_n-w"] a')
+            let curTitle = el.querySelector('div[class="video-card_n-w"] a')
             .title.replace(/[^a-z\u0400-\u04FF]/gi, " ")
             .trim().replace(/[\s]{1,}/g, ' ');
-            i++
+            
+            if (curTitle.length > 1) {
+                tarr[i] = curTitle;
+                i++;                
+            }
+
         }
     });
+    console.log(tarr);
     localStorage.setItem('oktag_names', JSON.stringify(tarr))
 };
