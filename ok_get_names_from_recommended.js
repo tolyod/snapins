15963621e4872a6a7e5008af429390df7c53a89e
anatoliy.poloz <1892972+tolyod@users.noreply.github.com>
2024-08-23 00:04:41 +0300

    Create ok_get_names_from_recommended.js

---
+++ ok_get_names_from_recommended.js
@@ -0,0 +1,13 @@
+javascript:function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+function saveTags() {
+  const videos = [...document.querySelectorAll("#listBlockPanelVideoUniversalContentBlock > div > div > div.ugrid_cnt .ugrid_i.js-seen-item-movie")];
+  const names = videos.map(e => e.querySelector('a.video-card_n.ellip').title);
+  const uniqNames = new Set(names);
+  localStorage.setItem('tags', JSON.stringify(uniqNames));
+}
+saveTags();
