a68eb85826a4b3afc96253e0eb8c414cc32b3835
anatoliy.poloz <1892972+tolyod@users.noreply.github.com>
2024-10-26 15:17:15 +0300

    Update ok_update_one.js

--- ok_update_one.js
+++ ok_update_one.js
@@ -1,4 +1,4 @@
-javascript:function shuffle(array) {
+javascript:function shuffleList(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -6,18 +6,18 @@ javascript:function shuffle(array) {
   return array;
 }
 
-function makeShot() {
+function makeShot(shuffleCb) {
   const separator = ' !!! ';
-  const tags = shuffle(JSON.parse(localStorage.getItem('tags')));
+  const tags = shuffleCb(JSON.parse(localStorage.getItem('tags')));
   const nameInput = document.getElementById('movie-title');
   const tagsInput = document.querySelector('input[name="st.ve.tags"]');
   const submitButton = document.querySelector('input[type="submit"].button-pro.js-submit-annotations-form');
   const curMovieName = nameInput.value.split(separator)[0];
   const nameValue = `${curMovieName}${separator}${tags.slice(1,5).join(' ')}`.slice(0, 150);
-  const tagsValue = shuffle(tags).slice(0,7).join(',');
+  const tagsValue = shuffleCb(tags).slice(0,7).join(',');
   
   nameInput.value = nameValue;
   tagsInput.value = tagsValue;
   submitButton.click();
 }
-makeShot();
+makeShot(shuffleList);
